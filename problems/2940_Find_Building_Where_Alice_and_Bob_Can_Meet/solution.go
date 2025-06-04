e2c988829b82e601ef71907deedb1e654506fd54
g82411 <andy.lin82411@gmail.com>
2025-06-04 10:59:29 +0800

    Remove external dependency and reimplement tree operations

--- problems/2940_Find_Building_Where_Alice_and_Bob_Can_Meet/solution.go
+++ problems/2940_Find_Building_Where_Alice_and_Bob_Can_Meet/solution.go
@@ -6,10 +6,7 @@ package _940_Find_Building_Where_Alice_and_Bob_Can_Meet
 // in query[i] {a, b} 找到第一個heights[k] > max(heights[a], heights[b])
 // 所以從最右邊開始處理 然後把處理過的heights 放到有序容器裡面
 
-import (
-	"github.com/emirpasic/gods/trees/redblacktree"
-	"sort"
-)
+import "sort"
 
 func leftmostBuildingQueries(heights []int, queries [][]int) []int {
 	// 先確保for all {a, b} in query[i] a < b
@@ -22,19 +19,17 @@ func leftmostBuildingQueries(heights []int, queries [][]int) []int {
 	sort.Slice(queries, func(i, j int) bool {
 		return queries[i][1] > queries[j][1]
 	})
-	// 有序集合怎摸辦, 直接送一顆紅黑樹
-	rbt := redblacktree.NewWithIntComparator()
 	rets := make([]int, len(queries))
+	stack := []int{}
 	i := len(heights) - 1
-	//
 	for _, query := range queries {
 		a, b, idx := query[0], query[1], query[2]
-		// 將在b右邊的東西都擺進有序集合裡面惹
+		// 將在 b 右邊的元素加入單調遞減 stack
 		for i >= b {
-			for !rbt.Empty() && heights[i] >= rbt.Left().Key.(int) {
-				rbt.Remove(rbt.Left().Key)
+			for len(stack) > 0 && heights[i] >= heights[stack[len(stack)-1]] {
+				stack = stack[:len(stack)-1]
 			}
-			rbt.Put(heights[i], i)
+			stack = append(stack, i)
 			i--
 		}
 		if heights[a] < heights[b] || a == b {
@@ -42,14 +37,14 @@ func leftmostBuildingQueries(heights []int, queries [][]int) []int {
 			continue
 		}
 		target := max(heights[a], heights[b])
-		f, found := rbt.Ceiling(target + 1)
-		if found {
-			rets[idx] = f.Value.(int)
-			continue
+		ansIdx := -1
+		for j := len(stack) - 1; j >= 0; j-- {
+			if stack[j] > b && heights[stack[j]] > target {
+				ansIdx = stack[j]
+				break
+			}
 		}
-		rets[idx] = -1
-		// 如果找到的元素等於目標值，則尋找下一個元素
-
+		rets[idx] = ansIdx
 	}
 	return rets
 }
